1ef81e930814ccd2321191417ba977e03795f67e
DvanessacelisG <46729805+DvanessacelisG@users.noreply.github.com>
2019-02-26 11:02:14 -0500

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -4,7 +4,7 @@ sudo apt-get -y install default-jre > /dev/null 2>&1
 
 npm install express express-jwt auth0-api-jwt-rsa-validation --save
 sudo apt install curl
-curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
-sudo apt install nodejs
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 echo "Success"
